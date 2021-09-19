261eaf46568de1b305005df6ce1568974eacba74
toanlqctk42 <toanlqctk42@gmail.com>
2021-09-19 14:58:28 +0700

    update

--- Lab01/bai1/luyentap2/bai3/cal.js
+++ Lab01/bai1/luyentap2/bai3/cal.js
@@ -1,12 +1,12 @@
 function f(d) {
-    if (d == 'c') {
+    if (d == 'c') { // nếu kí tự nhập vào là 'c' thì sẽ reset lại giá trị
     document.getElementById('res').value = "";
     return;
     }
     res = document.getElementById('res').value;
-    if (res == 0 && d == 0)
+    if (res == 0 && d == 0) // nếu giá trị ban đầu và giá trị nhập vào bằng 0 thì dừng tính toán
     return;
-    if (d == '+' || d == '-' || d == '*' || d == '/') {
+    if (d == '+' || d == '-' || d == '*' || d == '/') { // kiểm tra kí tự vừa nhập vào là số hay phép tính
     opr = d;
     num = parseFloat(res);
     document.getElementById('res').value = d;
@@ -14,13 +14,13 @@ function f(d) {
     }
     if (d == '=') {
     num1 = parseFloat(res);
-    switch (opr) {
+    switch (opr) { // thực thi phép tính
     case '+': ans = num + num1; break;
     case '-': ans = num - num1; break;
     case '*': ans = num * num1; break;
     case '/': ans = parseInt(num / num1); break;
     }
-    document.getElementById('res').value = ans;
+    document.getElementById('res').value = ans; // trả về giá trị phép tính
     return;
     }
     if (d == '--') {
