12855d3a7c2014182bc954508baa7dec892fc658
toanlqctk42 <toanlqctk42@gmail.com>
2021-11-26 23:51:16 +0700

    update

--- Lab07/views/employees/index.php
+++ Lab07/views/employees/index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>Example PHP+PDO+POO+MVC</title>
+    <title>Lê Quốc Toàn</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://kit.fontawesome.com/0c397bfc39.js" crossorigin="anonymous"></script>
 </head>
